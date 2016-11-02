d145f0d9152ec0589fca4b7b73bc8cec73c6ec40
izlatevski <ivan@ifkreativa.com>
2016-11-02 15:10:13 +0100

    after merge

--- www/js/ihr.js
+++ www/js/ihr.js
@@ -2,51 +2,53 @@
     var take = 3;
     var skipCount = getSkipCount();
     $.ajax({
-        url: "http://demo3.ifkreativa.com/api/getMessages?lang=mk&skip=" + skipCount + "&take=" + take + "",
+        url: "http://localhost:51766/api/getMessages?lang=mk&skip=" + skipCount + "&take=" + take + "",
         type: "GET",
         dataType: "jsonp",
         success: function (data) {
-            if(data["status"] == "OK")
-            var html = "";
-            $.each(data["messages"], function (index, el) {
-                html += "<div class='page-blog-list'>"
-                    + "<div class='page-blog-tags'><i class='ion-calendar date-icon'></i> Објавено на: " + el.publishDate + "</div><h4 class='page-blog-title'>" + el.title + "</h4>"
-                    + "<div class='page-blog-content'><p>" + el.description + "</p>"
-                + "</div><div class='clear'></div>"
-                        + "</div><div class='decoration'></div>";
-                        
-            });
-            $(".blog-posts").append(html);
-            if (!data["hasMore"])
-                $(".btn-get-events").hide();
+            if (data["status"] == "OK") {
+                var html = "";
+                $.each(data["messages"], function (index, el) {
+                    html += "<div class='page-blog-list'>"
+                        + "<div class='page-blog-tags'><i class='ion-calendar date-icon'></i> Објавено на: " + el.publishDate + "</div><h4 class='page-blog-title'>" + el.title + "</h4>"
+                        + "<div class='page-blog-content'><p>" + el.description + "</p>"
+                    + "</div><div class='clear'></div>"
+                            + "</div><div class='decoration'></div>";
+
+                });
+                $(".blog-posts").append(html);
+                if (!data["hasMore"])
+                    $(".btn-get-events").hide();
+            }
         }
     })
 }
 
-function getDocuments(type) {
+function getDocuments(_type) {
     var take = 3;
     var skipCount = getSkipCount();
-    var apiAction = type == "povrzani-dokumenti" ? "getDocuments" : "getReports";
+    var apiAction = _type == "povrzani-dokumenti" ? "getDocuments" : "getReports";
    
     $.ajax({
-        url: "http://demo3.ifkreativa.com/api/" + apiAction + "?skip=" + skipCount + "&take=" + take + "",
+        url: "http://localhost:51766/api/" + apiAction + "?skip=" + skipCount + "&take=" + take + "",
         type: "GET",
         dataType: "jsonp",
         success: function (data) {
-            if (data["status"] == "OK")
+            if (data["status"] == "OK") {
                 var html = "";
-            $.each(data["documents"], function (index, el) {
-                html += "<div class='page-blog-list'>"
-                    + "<div class='page-blog-tags'><i class='ion-calendar date-icon'></i> Објавено на: " + el.publishDate + "</div><h4 class='page-blog-title'>" + el.name + "</h4>"
-                    + " <div class='page-blog-list-by'> "
-                              + "<a href='http://demo3.ifkreativa.com" + el.link + "' class='button btn-download'><i class='ion-ios-download date-icon'></i> Превземи</a>"
-                          + "</div><div class='clear'></div>"
-                        + "</div><div class='decoration'></div>";
+                $.each(data["documents"], function (index, el) {
+                    html += "<div class='page-blog-list'>"
+                        + "<div class='page-blog-tags'><i class='ion-calendar date-icon'></i> Објавено на: " + el.publishDate + "</div><h4 class='page-blog-title'>" + el.name + "</h4>"
+                        + " <div class='page-blog-list-by'> "
+                                  + "<a href='http://localhost:51766" + el.link + "' class='button btn-download'><i class='ion-ios-download date-icon'></i> Превземи</a>"
+                              + "</div><div class='clear'></div>"
+                            + "</div><div class='decoration'></div>";
 
-            });
-            $(".blog-posts").append(html);
-            if (!data["hasMore"])
-                $(".btn-get-events").hide();
+                });
+                $(".blog-posts").append(html);
+                if (!data["HasMore"])
+                    $(".btn-get-events").hide();
+            }
         }
 
     });
