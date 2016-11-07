8187b12f5baaa09e7d9044e86218a2681c25e6d6
Filip Dimitrovski <filip@ifkreativa.com>
2016-11-07 12:26:16 +0100

    Structure changed

--- www/js/index.js
+++ www/js/index.js
@@ -1,138 +1,127 @@
 ﻿
-var app = {
-    // Application Constructor
-    initialize: function () {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        //alert('bindEvents');
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
-        app.receivedEvent('deviceready');
-    },
+function Initalize()
+{
+    bindEvents();
+}
+
+function bindEvents()
+{
+    document.addEventListener('deviceready', onDeviceReady, false);
+}
+
+function onDeviceReady()
+{
+    receivedEvent('deviceready');
+}
+
+function receivedEvent(id)
+{
+    console.log('Received Event: ' + id);
+    //LoadScripts();
+    //callHome();
+
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("backbutton", onBackKeyDown, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+    document.addEventListener("searchbutton", onSearchKeyDown, false);
+    document.addEventListener("offline", onOffline, false);
+    document.addEventListener("online", onOnline, false);
+    document.addEventListener("resume", onResume, false);
+
+    //var pushNotification = window.plugins.pushNotification;
+    //pushNotification.register(app.successHandler, app.errorHandler, { "senderID": "744083827880", "ecb": "app.onNotificationGCM" });
+}
 
 
+function onNotificationGCM(e) {
+    //alert(JSON.stringify(e));
+    //switch (e.event) {
+    //    case 'registered':
+    //        if (e.regid.length > 0) {
+    //                localStorage.registerID = e.regid;
+    //                $("#preloader").addClass("hide");
+    //                jQuery.ajax({
+    //                    url: "http://wazzzapp.net/Mobile/RegisterDevice?registerID=" + e.regid,
+    //                    type: "GET",
+    //                    dataType: "json",
+    //                    data: { registerID: e.regid },
+    //                    withCredentials: false,
+    //                    success: function (data, status) {
+    //                        //$("#preloader").addClass("hide");
+    //                    },
+    //                    error: function (error) {
+    //                    }
+    //                });
+    //            }
+    //        break;
 
-    // Update DOM on a Received Event
-    receivedEvent: function (id) {
+    //    case 'message':
+    //        //navigator.notification.alert(JSON.stringify(e), function () { }, "WazzzApp Frankfurt", "Ok");
+    //        navigator.notification.alert(e.payload.message, function () { }, e.payload.title, "Ok");
+    //        break;
 
-        console.log('Received Event: ' + id);
-        //LoadScripts();
-        //callHome();
+    //    case 'error':
+    //        alert('GCM error = ' + e.msg);
+    //        break;
 
-        document.addEventListener("pause", app.onPause, false);
-        document.addEventListener("backbutton", app.onBackKeyDown, false);
-        document.addEventListener("menubutton", app.onMenuKeyDown, false);
-        document.addEventListener("searchbutton", app.onSearchKeyDown, false);
-        document.addEventListener("offline", app.onOffline, false);
-        document.addEventListener("online", app.onOnline, false);
-        document.addEventListener("resume", app.onResume, false);
+    //    default:
+    //        alert('An unknown GCM event has occurred');
+    //        break;
+    //}
+}
 
-        //var pushNotification = window.plugins.pushNotification;
-        //pushNotification.register(app.successHandler, app.errorHandler, { "senderID": "744083827880", "ecb": "app.onNotificationGCM" });
+function onPause() {
+    // Handle the back button
+}
 
-    },
 
-    successHandler: function (result) {
-        //alert('Callback Success! Result = ' + result)
-    },
-
-    errorHandler: function (error) {
-        //alert(error);
-    },
-
-    onNotificationGCM: function (e) {
-        //alert(JSON.stringify(e));
-        //switch (e.event) {
-        //    case 'registered':
-        //        if (e.regid.length > 0) {
-        //                localStorage.registerID = e.regid;
-        //                $("#preloader").addClass("hide");
-        //                jQuery.ajax({
-        //                    url: "http://wazzzapp.net/Mobile/RegisterDevice?registerID=" + e.regid,
-        //                    type: "GET",
-        //                    dataType: "json",
-        //                    data: { registerID: e.regid },
-        //                    withCredentials: false,
-        //                    success: function (data, status) {
-        //                        //$("#preloader").addClass("hide");
-        //                    },
-        //                    error: function (error) {
-        //                    }
-        //                });
-        //            }
-        //        break;
-
-        //    case 'message':
-        //        //navigator.notification.alert(JSON.stringify(e), function () { }, "WazzzApp Frankfurt", "Ok");
-        //        navigator.notification.alert(e.payload.message, function () { }, e.payload.title, "Ok");
-        //        break;
-
-        //    case 'error':
-        //        alert('GCM error = ' + e.msg);
-        //        break;
-
-        //    default:
-        //        alert('An unknown GCM event has occurred');
-        //        break;
-        //}
-    },
-
-    onPause: function () {
-        // Handle the back button
-    },
-
-    onBackKeyDown: function (e) {
-        navigator.notification.alert("Back key down clicked", function () { }, "Порака", "Ok");
-        //e.preventDefault();
-        if (localStorage.page) {
-            if (localStorage.page == "homepage") {
-                navigator.notification.alert("Active page is: " + localStorage.page, function () { }, "Порака", "Ok");
-                if (confirm("Дали сте сигурни дека сакате да ја затворите апликацијата?")) {
-                    navigator.app.exitApp();
-                }
-            }
-            else {
-                navigator.notification.alert("Active page is: " + localStorage.page + ". User will be redirected", function () { }, "Порака", "Ok");
-                navigator.app.backHistory()
+function onBackKeyDown(e) {
+    alert("BackButton pressed");
+    navigator.notification.alert("Back key down clicked", function () { }, "Порака", "Ok");
+    //e.preventDefault();
+    if (localStorage.page) {
+        if (localStorage.page == "homepage") {
+            navigator.notification.alert("Active page is: " + localStorage.page, function () { }, "Порака", "Ok");
+            if (confirm("Дали сте сигурни дека сакате да ја затворите апликацијата?")) {
+                navigator.app.exitApp();
             }
         }
-    },
-
-    onMenuKeyDown: function () {
-        //Handle the menu key down button
-    },
-
-    onSearchKeyDown: function () {
-        // Handle the back button
-    },
-
-    onOffline: function () {
-        alert("You are offline now");
-        // Handle the offline event
-    },
-
-    onOnline: function () {
-        // Handle the online event
-        alert("You are online now");
-    },
-
-    onResume: function () {
-        // Handle resume event
-        //$("#preloader").addClass("hide");
+        else {
+            navigator.notification.alert("Active page is: " + localStorage.page + ". User will be redirected", function () { }, "Порака", "Ok");
+            navigator.app.backHistory()
+        }
     }
-};
-
-function GoBack()
-{
-
 }
+
+function onMenuKeyDown() {
+    //Handle the menu key down button
+}
+
+function onSearchKeyDown() {
+    // Handle the back button
+}
+
+function onOffline() {
+    alert("You are offline now");
+    // Handle the offline event
+}
+
+function onOnline() {
+    // Handle the online event
+    alert("You are online now");
+}
+
+function onResume() {
+    // Handle resume event
+    //$("#preloader").addClass("hide");
+}
+
+
+
+
+
+
+
+
+
+
---
+++ www/js/indexOld.js
@@ -0,0 +1,138 @@
+﻿
+var app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        //alert('bindEvents');
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function () {
+        app.receivedEvent('deviceready');
+    },
+
+
+
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+
+        console.log('Received Event: ' + id);
+        //LoadScripts();
+        //callHome();
+
+        document.addEventListener("pause", app.onPause, false);
+        document.addEventListener("backbutton", app.onBackKeyDown, false);
+        document.addEventListener("menubutton", app.onMenuKeyDown, false);
+        document.addEventListener("searchbutton", app.onSearchKeyDown, false);
+        document.addEventListener("offline", app.onOffline, false);
+        document.addEventListener("online", app.onOnline, false);
+        document.addEventListener("resume", app.onResume, false);
+
+        //var pushNotification = window.plugins.pushNotification;
+        //pushNotification.register(app.successHandler, app.errorHandler, { "senderID": "744083827880", "ecb": "app.onNotificationGCM" });
+
+    },
+
+    successHandler: function (result) {
+        //alert('Callback Success! Result = ' + result)
+    },
+
+    errorHandler: function (error) {
+        //alert(error);
+    },
+
+    onNotificationGCM: function (e) {
+        //alert(JSON.stringify(e));
+        //switch (e.event) {
+        //    case 'registered':
+        //        if (e.regid.length > 0) {
+        //                localStorage.registerID = e.regid;
+        //                $("#preloader").addClass("hide");
+        //                jQuery.ajax({
+        //                    url: "http://wazzzapp.net/Mobile/RegisterDevice?registerID=" + e.regid,
+        //                    type: "GET",
+        //                    dataType: "json",
+        //                    data: { registerID: e.regid },
+        //                    withCredentials: false,
+        //                    success: function (data, status) {
+        //                        //$("#preloader").addClass("hide");
+        //                    },
+        //                    error: function (error) {
+        //                    }
+        //                });
+        //            }
+        //        break;
+
+        //    case 'message':
+        //        //navigator.notification.alert(JSON.stringify(e), function () { }, "WazzzApp Frankfurt", "Ok");
+        //        navigator.notification.alert(e.payload.message, function () { }, e.payload.title, "Ok");
+        //        break;
+
+        //    case 'error':
+        //        alert('GCM error = ' + e.msg);
+        //        break;
+
+        //    default:
+        //        alert('An unknown GCM event has occurred');
+        //        break;
+        //}
+    },
+
+    onPause: function () {
+        // Handle the back button
+    },
+
+    onBackKeyDown: function (e) {
+        navigator.notification.alert("Back key down clicked", function () { }, "Порака", "Ok");
+        //e.preventDefault();
+        if (localStorage.page) {
+            if (localStorage.page == "homepage") {
+                navigator.notification.alert("Active page is: " + localStorage.page, function () { }, "Порака", "Ok");
+                if (confirm("Дали сте сигурни дека сакате да ја затворите апликацијата?")) {
+                    navigator.app.exitApp();
+                }
+            }
+            else {
+                navigator.notification.alert("Active page is: " + localStorage.page + ". User will be redirected", function () { }, "Порака", "Ok");
+                navigator.app.backHistory()
+            }
+        }
+    },
+
+    onMenuKeyDown: function () {
+        //Handle the menu key down button
+    },
+
+    onSearchKeyDown: function () {
+        // Handle the back button
+    },
+
+    onOffline: function () {
+        alert("You are offline now");
+        // Handle the offline event
+    },
+
+    onOnline: function () {
+        // Handle the online event
+        alert("You are online now");
+    },
+
+    onResume: function () {
+        // Handle resume event
+        //$("#preloader").addClass("hide");
+    }
+};
+
+function GoBack()
+{
+
+}
